a7eee63f4e149bc7d9d87dfd6fa2ce5595440bdf
CoLoDot <colodotgo@gmail.com>
2020-02-18 11:34:08 +0100

    Add alert component'

--- leiamanager/leia_ui/src/components/App.js
+++ leiamanager/leia_ui/src/components/App.js
@@ -1,22 +1,34 @@
 import React, { Component, Fragment } from "react";
 import ReactDom from "react-dom";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 import Header from "./layout/Header";
 import Dashboad from "./leaders/Dashboard";
+import Alerts from "./layout/Alerts";
 
 import { Provider } from "react-redux";
 import store from "../store";
+
+// Alerting options
+const alertingOptions = {
+  timeout: 3000,
+  position: "top center"
+};
 
 class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboad />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertingOptions}>
+          <Fragment>
+            <Header />
+            <Alerts />
+            <div className="container">
+              <Dashboad />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
