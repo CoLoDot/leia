03030e999063b1c5e199e286b2d7f3c0f7e6b166
CoLoDot <constancelouise.gauriau@gmail.com>
2020-03-29 22:14:49 +0200

    :recycle: Update constants

--- leiamanager/leia/constants.py
+++ leiamanager/leia/constants.py
@@ -1,15 +1,21 @@
 #!/usr/bin/python3
+import sys
 
 # Wikipedia api url
-URL = "https://en.wikipedia.org/w/api.php"
+URL = "https://query.wikidata.org/sparql"
+
+# Set a user Agent to request Wikidata
+USER_AGENT = "Leia/%s.%s" % (
+    sys.version_info[0], sys.version_info[1])
 
 # Wikipedia params for requests list of taxon categories
-CATEGORIES_PARAMS = {
-    "action": "query",
-    "cmtitle": "Category:IUCN Red List extinct species",
-    "cmlimit": "8",
-    "cmstartsortkeyprefix": "*",
-    "cmprop": "ids|sortkeyprefix|title",
-    "list": "categorymembers",
-    "format": "json"
-}
+WIKIDATA_QUERY = """SELECT ?esp_ce__teinte ?esp_ce__teinteLabel ?nom_scientifique_du_taxon ?taxon_sup_rieurLabel ?rang_taxinomiqueLabel ?image ?end_mique_deLabel WHERE {
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+  ?esp_ce__teinte wdt:P141 wd:Q237350.
+  FILTER EXISTS { ?esp_ce__teinte wdt:P18 ?image. }
+  OPTIONAL { ?esp_ce__teinte wdt:P225 ?nom_scientifique_du_taxon. }
+  OPTIONAL { ?esp_ce__teinte wdt:P171 ?taxon_sup_rieur. }
+  OPTIONAL { ?esp_ce__teinte wdt:P105 ?rang_taxinomique. }
+  OPTIONAL { ?esp_ce__teinte wdt:P18 ?image. }
+  OPTIONAL { ?esp_ce__teinte wdt:P183 ?end_mique_de. }
+}"""
