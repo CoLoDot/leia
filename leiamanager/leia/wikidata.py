1ae2edc530a6d3993c285fc145ed3d9041aa6a13
CoLoDot <constancelouise.gauriau@gmail.com>
2020-03-29 22:15:34 +0200

    :sparkles: Getting data

--- leiamanager/leia/wikidata.py
+++ leiamanager/leia/wikidata.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-from .get_taxon_categories import TaxonCategories
+from .get_taxon_categories import Taxon
 
 
 def update_model_Taxon():
-    taxon = TaxonCategories()
-    taxon.get_taxon_categories()
+    taxon = Taxon()
+    taxon.get_sparql_wikidata()
+    taxon.get_taxon_data()
+    taxon.insert_taxon_data()
