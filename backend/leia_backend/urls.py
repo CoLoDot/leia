439a78887b3331f117b12dce06584d0f4a2840c1
CoLoDot <constancelouise.gauriau@gmail.com>
2020-05-02 00:29:39 +0200

    :bug: Fix frontend deployment

--- backend/leia_backend/urls.py
+++ backend/leia_backend/urls.py
@@ -1,5 +1,6 @@
 """leia_backend URL Configuration"""
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 
 def trigger_error(request):
     division_by_zero = 1 / 0
@@ -7,4 +8,5 @@ def trigger_error(request):
 urlpatterns = [
     path('', include('leia_api.urls')),
     path('sentry-debug/', trigger_error),
+    re_path(".*", TemplateView.as_view(template_name="index.html")),
 ]
