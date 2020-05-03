1fef57d167255ec4874be3acfc6765eb202617e1
CoLoDot <constancelouise.gauriau@gmail.com>
2020-05-03 11:41:57 +0200

    :bug: Load inital data

--- backend/scripts/entrypoint-production.sh
+++ backend/scripts/entrypoint-production.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 python backend/manage.py makemigrations --no-input
 python backend/manage.py migrate --no-input
-python backend/manage.py loaddata leia_api/dumps/leia_api.json
+python backend/manage.py loaddata backend/leia_api/dumps/leia_api.json
 python backend/manage.py runserver 0.0.0.0:$PORT
